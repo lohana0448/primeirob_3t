157bf41423b756a4b5202feacdf880aa52bed813
lohana0448 <110116092+lohana0448@users.noreply.github.com>
2022-10-07 09:07:36 -0300

    07/10

--- estudo.js
+++ estudo.js
@@ -32,3 +32,17 @@ function multiplica(){
     }
 }
 
+function total(){
+let v = document.getElementById("valor").value;
+let j = document.getElementById("juros").value;
+let t = document.getElementById("meses").value;
+let r = 0;
+for (let i=1; i <= t; i++) {
+ r = v * (1 + (j/100));
+ document.write("Mes:" + i + "valor:" + r +"<br>" );
+ v = r;
+
+}
+document.write("Resultado:" + r);
+
+}
